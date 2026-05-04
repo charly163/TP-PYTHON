c4171342260eafae5ad0df9d5790a2bf45fa4e86
FabriGio87 <fabristraits@gmail.com>
2026-05-04 00:39:48 -0300

    Agrego mi aporte

---
+++ Giovagnoli/fabri/mi_aporte.py
@@ -0,0 +1,9 @@
+print("Hola profe 😎")
+print("Si este código funciona, no lo toques...")
+
+numero = 13
+
+if numero > 5:
+    print("Número mayor a 5 ✔")
+else:
+    print("Número menor o igual a 5 ❌")
